11bd072f5faede9f34c75d910a30d4d9cb177f33
zelazna <constantin.guidon@gmail.com>
2016-05-11 12:58:39 +0200

    regex

--- models/account.js
+++ models/account.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
-//@todo change username for email and regex it for @hetic.net
+//@todo change username for email and include the regex for @hetic.net
 var Account = new Schema({
     username: String,
     password: String
--- regex.js
+++ regex.js
@@ -1,3 +1,3 @@
-var url = 'constantin.guidon@hetic.net';
+var url = 'tutu.toto@hetic.net';
 var regex = /[a-z@\+.]{2,10}\.[a-z]{2,9}@hetic.net/;
 console.log(regex.test(url));
